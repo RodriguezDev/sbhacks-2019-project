88f7cf2ef5c945a5cdfdc7c1849046c5c9bb18bc
Zain <zalind@gmail.com>
2019-01-12 03:59:23 -0800

    Setup project

--- app/src/main/java/com/project/sbhacks/sbhacks_2019_project/MainActivity.java
+++ app/src/main/java/com/project/sbhacks/sbhacks_2019_project/MainActivity.java
@@ -1,13 +1,141 @@
 package com.project.sbhacks.sbhacks_2019_project;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+import com.google.android.gms.location.LocationRequest;
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.ArSceneView;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import uk.co.appoly.arcorelocation.LocationMarker;
+import uk.co.appoly.arcorelocation.LocationScene;
+import uk.co.appoly.arcorelocation.utils.ARLocationPermissionHelper;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final double MIN_OPENGL_VERSION = 3.0;
+
+    private ArFragment arFragment;
+    private ModelRenderable pinRenderable;
+
+    private LocationScene locationScene;
 
     @Override
+    @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (!checkIsSupportedDeviceOrFinish(this)) {
+            return;
+        }
+
         setContentView(R.layout.activity_main);
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+
+        ModelRenderable.builder()
+                .setSource(this, Uri.parse("pin.sfb"))
+                .build()
+                .thenAccept(renderable -> pinRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Toast toast =
+                                    Toast.makeText(this, "Unable to load pin renderable", Toast.LENGTH_LONG);
+                            toast.setGravity(Gravity.CENTER, 0, 0);
+                            toast.show();
+                            return null;
+                        });
+
+        arFragment.setOnTapArPlaneListener(
+                (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
+                    if (pinRenderable == null) {
+                        return;
+                    }
+
+                    Anchor anchor = hitResult.createAnchor();
+                    AnchorNode anchorNode = new AnchorNode(anchor);
+                    anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+                    TransformableNode pin = new TransformableNode(arFragment.getTransformationSystem());
+                    pin.setParent(anchorNode);
+                    pin.setRenderable(pinRenderable);
+                    pin.select();
+                });
+
+        if (!ARLocationPermissionHelper.hasPermission(this)) {
+            ARLocationPermissionHelper.requestPermission(this);
+        }
+
+        arFragment
+                .getArSceneView()
+                .getScene()
+                .addOnUpdateListener(frameTime -> {
+                    Log.d(TAG, "Hello");
+                    if (locationScene == null) {
+                        locationScene = new LocationScene(this, this, arFragment.getArSceneView());
+                        locationScene.mLocationMarkers.add(
+                                new LocationMarker(
+                                        -119.844045,
+                                        34.412954,
+                                        getPin()));
+                    }
+
+                    if (locationScene != null) {
+                        locationScene.processFrame(arFragment.getArSceneView().getArFrame());
+                    }
+                });
+    }
+
+    public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            Log.e(TAG, "Sceneform requires Android N or later");
+            Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show();
+            activity.finish();
+            return false;
+        }
+        String openGlVersionString =
+                ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
+                        .getDeviceConfigurationInfo()
+                        .getGlEsVersion();
+        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
+            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+                    .show();
+            activity.finish();
+            return false;
+        }
+        return true;
+    }
+
+    private Node getPin() {
+        Node base = new Node();
+        base.setRenderable(pinRenderable);
+        Context c = this;
+        base.setOnTapListener((v, event) -> {
+            Toast.makeText(
+                    c, "Pin touched.", Toast.LENGTH_LONG)
+                    .show();
+        });
+        return base;
     }
 }
